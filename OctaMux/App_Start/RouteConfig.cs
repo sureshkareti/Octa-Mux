24c8701d63feb77de19acdf84213ac095f414a18
Madhubabu <madhubabu559@gmail.com>
2018-08-18 20:28:50 +0530

    Fresh Setup

--- OctaMux/App_Start/RouteConfig.cs
+++ OctaMux/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace OctaMux
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "OM", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
